66bf01fff096dec36900b6532dcced88d74665f5
Saket <sakettiwari076@gmail.com>
2024-06-01 00:35:30 +0530

    ss

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import './App.css';
-import React, { createContext } from 'react';
-import ChildA from './ChildA';
-import Array from './Array';
+import React, { useEffect, useState } from 'react';
 
-const data=createContext();
-const data1=createContext();
-//create context --> provider --->consumer
-function App() {
-  const name="saket";
-  const gender = "male";
-  return (
-    <data.Provider value={name} >
-      <data1.Provider value = {gender}>
-    <ChildA/>
-    <Array/>
-    </data1.Provider>
-    </data.Provider>
+const App = () => {
+    const [restaurants, setRestaurants] = useState([]);
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+              const response = await fetch('https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.969541861336673&lng=77.69190827830462&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING');
+                const de = await response.json();
+                console.log(de);
+                const restaurantsData = de?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants;
+                console.log(restaurantsData);
+                    setRestaurants(restaurantsData);
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        };
 
-    
-  );
-}
+        fetchData();
+    }, []);
+
+    return (
+        <div>
+            <h1>Swiggy Restaurants</h1>
+            <div id="restaurants">
+                {restaurants?.length > 0 ? (
+                    restaurants.map((restaurant, index) => (
+                        <div key={index} className="restaurant">
+                            <h2>{restaurant.info.name}</h2>
+                            <p>{restaurant.info.avgRating}</p>
+
+
+                            {/* <p>Cuisine: {restaurant..cuisine.join(', ')}</p>
+                            <p>Rating: {restaurant.info.rating}</p> */}
+                        </div>
+                    ))
+                ) : (
+                    <p>No restaurants available</p>
+                )}
+            </div>
+        </div>
+    );
+};
 
 export default App;
-export {data,data1} ;
+
+    
